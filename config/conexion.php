4375a973b2c171f8dda3ad68dac05c35d3363eda
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-10 19:24:44 -0300

    asignacion de variables de conexion y rediccionamiento a /login

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST= 'roundhouse.proxy.rlwy.net';
-$DB_DATABASE= 'railway';
-$DB_USER= 'postgres';
-$DB_PASSWORD= 'CCcBfgEGBefeBFDCF1af2aD3fDD-e52e';
+$host= 'localhost';
+$bd= 'a_traves_de_mis_ojos';
+$user= 'postgres';
+$pass= '1234';
 
-$conexion=pg_connect("host=$DB_HOST dbname=$DB_DATABASE user=$DB_USER password=$DB_PASSWORD");
+$conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
 ?>

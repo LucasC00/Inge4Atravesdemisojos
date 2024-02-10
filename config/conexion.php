a4c50a24818939221034dce3a93f21e5d5fb9584
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-10 20:08:49 -0300

    ajuste variable conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host= 'localhost';
-$bd= 'a_traves_de_mis_ojos';
-$user= 'postgres';
-$pass= '1234';
+$DB_HOST= 'localhost';
+$DB_DATABASE= 'a_traves_de_mis_ojos';
+$DB_USER= 'postgres';
+$DB_PASSWORD= '1234';
 
-$conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
+$conexion=pg_connect("host=$DB_HOST dbname=$DB_DATABASE user=$DB_USER password=$DB_PASSWORD");
 ?>

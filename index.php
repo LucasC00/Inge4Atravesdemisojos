08b5c38d01fc6274f05ea881e552cec174464f36
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-11 18:11:55 -0300

    apunta a vista

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Redirigir al usuario a la página de login
-header("Location: /login");
+header("Location: /src/Views/login.php");
 exit; // Asegúrate de terminar el script después de la redirección
 ?>

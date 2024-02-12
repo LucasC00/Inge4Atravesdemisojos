ea33c1a84f91eda20205d2f8eea5c986a021d6df
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-12 06:04:07 -0300

    se importa el DB y se modifica el metodo para autenticar

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; // Importa la clase Auth
+use Illuminate\Support\Facades\DB; // Importa la clase Auth
 
 class LoginController extends Controller
 {
@@ -15,18 +16,26 @@ class LoginController extends Controller
     public function autenticarUsuario(Request $request)
     {
         // Recibe los datos del formulario enviado por POST
-        $credenciales = $request->only('usuario', 'password');
-
-        // Intenta autenticar al usuario usando el sistema de autenticación de Laravel
-        if (Auth::attempt($credenciales)) {
-            // Si la autenticación es exitosa, guarda la información del usuario en la sesión
-            $request->session()->regenerate(); // Regenera la sesión para evitar ataques de sesión falsa
-            $request->session()->put('usuario', Auth::user());
-            // Si la autenticación es exitosa, redirige al usuario a la página de inicio
-            return redirect()->route('home');
-        } else {
-            // Si la autenticación falla, redirige de vuelta al formulario de inicio de sesión con un mensaje de error
-            return redirect()->route('login')->with('error', 'Credenciales incorrectas');
+        $usuario = $request->input('usuario');
+        $clave = $request->input('password');
+    
+        // Realiza la consulta SQL personalizada
+        $usuarioData = DB::select("SELECT * FROM usuarios WHERE nombre_usuario = '$usuario'");
+    
+        // Verifica si se encontró un usuario con el nombre proporcionado
+        if ($usuarioData) {
+            $usuarioData = $usuarioData[0]; // Obtén el primer resultado (debería ser único)
+            // Verificar la contraseña
+            if (password_verify($clave, $usuarioData->contrasena)) {
+                // Si la contraseña es correcta, realiza la autenticación
+                // Por ejemplo, puedes guardar el usuario en la sesión
+                session(['usuario' => $usuarioData]);
+                // Redirige al usuario a la página de inicio
+                return redirect()->route('home');
+            }
         }
+    
+        // Si no se encuentra un usuario o la contraseña es incorrecta, redirige de vuelta al formulario de inicio de sesión con un mensaje de error
+        return redirect()->route('login')->with('error', 'Credenciales incorrectas');
     }
 }

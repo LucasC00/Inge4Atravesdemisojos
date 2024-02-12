1fd9311f1021d48633af4f21e23bb0d657054bf8
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-12 08:20:06 -0300

    se cambia la forma de tomar los datos del form de registro

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -16,33 +16,31 @@ class RegistroController extends Controller
     public function registrarUsuario(Request $request)
     {
         // Validamos los datos del formulario
-        $request->validate([
-            'nombreUsuario' => 'required|unique:usuarios,nombre_usuario',
-            'nombreReal' => 'required',
-            'correo' => 'required|email|unique:usuarios,correo',
-            'contrasena' => 'required',
-            'discapacidadVisual' => 'nullable|boolean',
-            'sexo' => 'required',
-            'edad' => 'nullable|integer',
-            'biografia' => 'nullable|string',
-            'telefono' => 'nullable|string',
-            'ubicacion' => 'nullable|string',
-            'rol' => 'required|numeric',
-        ]);
+        $nombreUsuario = $request->input('nombreUsuario');
+        $nombreReal = $request->input('nombreReal');
+        $correo = $request->input('correo');
+        $contrasena = $request->input('contrasena');
+        $discapacidadVisual = $request->has('discapacidadVisual') ? true : false;
+        $sexo = $request->input('sexo');
+        $edad = $request->input('edad');
+        $numero_telefono = $request->input('biografia');
+        $telefono = $request->input('telefono');
+        $ubicacion = $request->input('ubicacion');
+        $rol = $request->input('rol');
 
         // Insertar el nuevo usuario en la base de datos
         $result = DB::table('usuarios')->insert([
-            'nombre_usuario' => $request->nombreUsuario,
-            'nombre_real' => $request->nombreReal,
-            'correo' => $request->correo,
-            'contrasena' => Hash::make($request->contrasena), // Se hashea la contraseña antes de almacenarla
-            'discapacidad_visual' => $request->has('discapacidadVisual') ? true : false, // Convierte a booleano
-            'sexo' => $request->sexo,
-            'edad' => $request->edad,
-            'biografia' => $request->biografia,
-            'numero_telefono' => $request->telefono,
-            'ubicacion' => $request->ubicacion,
-            'id_rol' => $request->rol,
+            'nombre_usuario' => $nombreUsuario,
+            'nombre_real' => $nombreReal,
+            'correo' => $correo,
+            'contrasena' => Hash::make($contrasena), // Se hashea la contraseña antes de almacenarla
+            'discapacidad_visual' => $discapacidadVisual, // Convierte a booleano
+            'sexo' => $sexo,
+            'edad' => $edad,
+            'biografia' => $biografia,
+            'numero_telefono' => $telefono,
+            'ubicacion' => $ubicacion,
+            'id_rol' => $rol,
             'fecha_registro' => now(), // Utiliza la fecha y hora actual
         ]);
 

83fd9035aa2926ba836c4472ed039e4abd374950
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-11 23:03:00 -0300

    ajuste en ruta

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 

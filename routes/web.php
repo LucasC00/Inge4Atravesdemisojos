f40a500d644a0092466e6e288cb59c8acec392d4
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-12 05:40:24 -0300

    ajuste en ruta de login y registro en login.php

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::get('/home', [HomeController::class, 'showHome'])->name('home');
 
 // Rutas LoginController
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login-autenticar', [LoginController::class, 'autenticarUsuario'])->name('login.autenticar');
+Route::post('/login-autenticar', [LoginController::class, 'autenticarUsuario'])->name('login-autenticar');
 
 // Rutas RegistroController
-Route::get('/registro-usuario', [RegistroController::class, 'showRegistro'])->name('registro.usuario');
-Route::post('/registro-realizar', [RegistroController::class, 'registrarUsuario'])->name('registro.realizar');
+Route::get('/registro-usuario', [RegistroController::class, 'showRegistro'])->name('registro-usuario');
+Route::post('/registro-realizar', [RegistroController::class, 'registrarUsuario'])->name('registro-realizar');

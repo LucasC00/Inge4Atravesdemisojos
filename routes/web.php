d78e8c30ebf3c38e390873eebf8a25a25d98e65e
LucasC00 <lucascorvalanb2@gmail.com>
2024-02-11 22:47:13 -0300

    se agrega declaracion gomecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
+use app\Http\Controllers\LoginController;
+use app\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 
